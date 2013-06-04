63f4c278bf34be1e3dec68a0ac20febf186c3565
eldoth <leonardowada@hotmail.com>
2013-06-04 20:23:05 -0300

    Bresenham para elipse

--- bresenham/bresenham_elipse.c
+++ bresenham/bresenham_elipse.c
@@ -16,9 +16,7 @@ typedef struct ponto {
 } ponto;
 
 float pointSize;
-ponto foco1, foco2;
 int raiox, raioy;
-int primeiroPonto;
 
 void init(void) {
 	glClearColor(1.0, 1.0, 1.0, 0.0);
@@ -32,131 +30,104 @@ void setPixel(GLint x, GLint y) {
 	glEnd();
 }
 
+int pXInicial() {
+	double result = pow(raioy, 2) - pow(raiox, 2) * raioy + (double)pow(raiox, 2) / 4;
+	return result;
+}
+
+int pYInicial(ponto ponto) {
+	double result = (pow(raioy, 2) * pow(ponto.x + ((double)1 / 2), 2))
+			+ (pow(raiox, 2) * pow(ponto.y - 1, 2))
+			- (pow(raioy, 2) * pow(raiox, 2));
+	return result;
+}
+
 /**
- * Método que verifica se o ponto está dentro, fora ou faz parte da elipse.
+ * o ponto que deve ser passado é o p(k+1).
  */
-int estaNaElipse(ponto ponto) {
-	int result = (pow(raioy, 2) * pow(ponto.x, 2))
-			+ (pow(raiox, 2) * pow(ponto.y, 2))
-			- (pow(raiox, 2) * pow(raioy, 2));
-	if (result == 0) {
-		// Ponto faz parte da elipse
-		return 0;
-	} else if (result > 0) {
-		// Ponto está fora da elipse
-		return 1;
+int calculaPx(ponto ponto, int pk) {
+	int pkMaisUm;
+	if (pk < 0) {
+		pkMaisUm = pk + pow(raioy, 2) * (2 * ponto.x + 1);
+	} else {
+		pkMaisUm = pk + pow(raioy, 2) * (2 * ponto.x + 1)
+				+ pow(raiox, 2) * (-2 * ponto.y);
 	}
-	// Ponto está dentro da elipse
-	return -1;
+	return pkMaisUm;
+}
+
+int calculaPy(int yk, int xkMaisUm, int pk) {
+	int pkMaisUm;
+	if (pk > 0) {
+		pkMaisUm = pk + pow(raiox, 2) * ((-2 * yk) + 1);
+	} else {
+		pkMaisUm = pk + pow(raioy, 2) * (2 * xkMaisUm)
+				+ pow(raiox, 2) * ((-2 * yk) + 1);
+	}
+	return pkMaisUm;
 }
 
 double getInclinacaoDaReta(ponto ponto) {
-	double m = - ((2 * pow(raioy, 2) * ponto.x) / (2 * pow(raiox, 2) * ponto.y));
-	return m;
-}
-
-int estaNoLimiteRegiaoUmDois(ponto ponto) {
-	if (2 * pow(raioy, 2) * ponto.x == 2 * pow(raiox, 2) * ponto.y) {
-		return 1;
+	if (ponto.y == 0) {
+		return 0.0;
 	}
-	return 0;
+	double m = -((double) (2 * pow(raioy, 2) * ponto.x) / (2 * pow(raiox, 2) * ponto.y));
+	return m;
 }
 
 void bresenham_elipse() {
 	ponto ponto;
 	ponto.x = 0;
 	ponto.y = raioy;
+	int pk;
+	int yk;
+	double m;
 	glClearColor(0.0, 0.0, 0.0, 0.0);
 	glClear(GL_COLOR_BUFFER_BIT);
 	glPointSize(pointSize);
-	while (!estaNoLimiteRegiaoUmDois(ponto)) {
+	pk = pXInicial();
+	setPixel(ponto.x, ponto.y);
+	m = getInclinacaoDaReta(ponto);
+	while (m >= -1) {
+		if (pk < 0) {
+			ponto.x++;
+		} else {
+			ponto.x++;
+			ponto.y--;
+		}
 		setPixel(ponto.x, ponto.y);
-		ponto.x++;
+		m = getInclinacaoDaReta(ponto);
+		pk = calculaPx(ponto, pk);
 	}
-	while(ponto.y >= 0) {
-		setPixel(ponto.x,ponto.y);
-		ponto.y++;
+	pk = pYInicial(ponto);
+	while (m <= -1) {
+		if (pk > 0) {
+			ponto.y--;
+		} else {
+			ponto.x++;
+			ponto.y--;
+		}
+		setPixel(ponto.x, ponto.y);
+		m = getInclinacaoDaReta(ponto);
+		yk = ponto.y;
+		pk = calculaPy(yk--, ponto.x, pk);
 	}
 	glFlush();
 }
 
-/*void Keyboard(unsigned char key, int x, int y) {
- switch (key) {
- case '0':
- glColor3f(1.0, 1.0, 1.0);
- break;
- case '1':
- glColor3f(1.0, 1.0, 1.0);
- break;
- case '2':
- glColor3f(1.0, 0.0, 0.0);
- break;
- case '3':
- glColor3f(0.0, 1.0, 0.0);
- break;
- case '4':
- glColor3f(1.0, 1.0, 0.0);
- break;
- case '5':
- glColor3f(1.0, 0.0, 1.0);
- break;
- case '6':
- glColor3f(0.0, 1.0, 1.0);
- break;
- case '7':
- glColor3f(1.0, 1.0, 0.5);
- break;
- case '8':
- int primeiroPonto;
- glColor3f(1.0, 0.5, 1.0);
- break;
- case '9':
-
- glColor3f(0.5, 1.0, 1.0);
- break;
- }
- glFlush();
- glutPostRedisplay();
- }*/
-
-/*void MouseInt(int botao, int estado, int x, int y) {
- if (primeiroPonto) {
- switch (botao) {
- case GLUT_LEFT_BUTTON:
- if (estado == GLUT_DOWN) {
- primeiroPonto--;
- p1.x = x - 300;
- p1.y = (y - 300) * (-1);
- break;
- }
- }
- } else {
- switch (botao) {
- case GLUT_LEFT_BUTTON:
- if (estado == GLUT_DOWN) {
- primeiroPonto++;
- p2.x = x - 300;
- p2.y = (y - 300) * (-1);
- glutDisplayFunc(bresenham);
- glutPostRedisplay();
- break;
- }
- }
- }
- }*/
-
 int main(int argc, char *argv[]) {
-	primeiroPonto = 1;
 	pointSize = 5.0;
-	foco1 =
+	//Parametros iniciais.
+	raioy = 250;
+	raiox = 150;
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
 	glutInitWindowPosition(50, 100);
 	glutInitWindowSize(600, 600);
 	glutInit(&argc, argv);
-	glutCreateWindow("Lista OpenGL 26_05 Exercicio 1");
+	glutCreateWindow("Lista OpenGL 06/06");
 	init();
 	glColor3f(0.0, 0.0, 1.0);
-	glutDisplayFunc(bresenham_elipse());
+	glutDisplayFunc(bresenham_elipse);
 	glutMainLoop();
 	return 0;
 }
